dcb985520ff037d19a81d738601d727aad040bcb
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2024-12-16 22:08:25 +0530

    Add answer to local storage and it can be not be go until clear button press

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 //Wrap code in an IIFE
-(function(){
-  
-  let screen = document.querySelector('.screen');
-  let buttons = document.querySelectorAll('.btn');
-  let clear = document.querySelector('.btn-clear');
-  let equal = document.querySelector('.btn-equal');
-  
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
+
+  const values = JSON.parse(localStorage.getItem("values")) || [];
   //retrieve data from numbers that are clicked
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
       let value = e.target.dataset.num;
       screen.value += value;
-    })
+
+      // if (e.target.dataset.ope) {
+      //   screen.value = e.target.dataset.ope;
+      // }
+    });
+    setLocal();
+    pickFromLocal();
+    renderAnswer();
   });
-  
-  equal.addEventListener('click', function(e){
-    if(screen.value === ''){
-      screen.value = 'Please Enter a Value';
+  values.forEach((valu) => pickFromLocal(valu));
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "Please Enter a Value";
     } else {
       let answer = eval(screen.value);
       screen.value = answer;
+      values.push(answer);
+      pickFromLocal(answer);
     }
-  })
-  
-  clear.addEventListener('click', function(e){
-    screen.value = '';
-  })
- 
+    setLocal();
+    pickFromLocal();
+    renderAnswer();
+  });
+  function pickFromLocal(valu) {
+    console.log(valu);
+  }
+  function renderAnswer() {
+    screen.value = values;
+  }
+  clear.addEventListener("click", function (e) {
+    screen.value = "";
+    localStorage.clear(values.splice(0));
+  });
+  function setLocal() {
+    localStorage.setItem("values", JSON.stringify(values));
+  }
 })(); //end IIFE
